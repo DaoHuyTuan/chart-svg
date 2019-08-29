30b00b2f75462515b085e24750833a4258c21c5b
Tuan Dao <tuan.dao@dinovative.com>
2019-08-29 13:52:39 +0700

    update

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -6,20 +6,36 @@ class Chart extends React.Component {
         const heightSVG = 500;
         const widthColums = 45;
         return (
-            <svg className="charts">
-                <g className="column" >
-                    <rect className="column__rect" x={0} y={heightSVG - 120} height="120"></rect>
-                    <text className="column__text" x={0} y={heightSVG - 130} > 120</text>
-                </g>
-                <g className="column">
-                    <rect className="column__rect" x={widthColums} y={heightSVG - 250} height="250"></rect>
-                    <text className="column__text" x={widthColums} y={heightSVG - 280} >250</text>
-                </g>
-                <g className="column">
-                    <rect className="column__rect" x={widthColums * 2} y={heightSVG - 400} height="400"></rect>
-                    <text className="column__text" x={widthColums * 2} y={heightSVG - 430}  >400</text>
-                </g>
-            </svg>
+	<>
+          <svg className="charts">
+          <g className="column" >
+          <rect className="column__rect" x={0} y={heightSVG - 120} height="120"></rect>
+          <text className="column__text" x={0} y={heightSVG - 130} > 120</text>
+          </g>
+          <g className="column">
+          <rect className="column__rect" x={widthColums} y={heightSVG - 250} height="250"></rect>
+          <text className="column__text" x={widthColums} y={heightSVG - 280} >250</text>
+          </g>
+          <g className="column">
+          <rect className="column__rect" x={widthColums * 2} y={heightSVG - 400} height="400"></rect>
+          <text className="column__text" x={widthColums * 2} y={heightSVG - 430}  >400</text>
+          </g>
+          </svg>
+	  <svg className="charts">
+          <g className="column" >
+          <rect className="column__rect" x={0} y={heightSVG - 120} height="120"></rect>
+          <text className="column__text" x={0} y={heightSVG - 130} > 120</text>
+          </g>
+          <g className="column">
+          <rect className="column__rect" x={widthColums} y={heightSVG - 250} height="250"></rect>
+          <text className="column__text" x={widthColums} y={heightSVG - 280} >250</text>
+          </g>
+          <g className="column">
+          <rect className="column__rect" x={widthColums * 2} y={heightSVG - 400} height="400"></rect>
+          <text className="column__text" x={widthColums * 2} y={heightSVG - 430}  >400</text>
+          </g>
+          </svg>
+</>
         )
     }
 }
